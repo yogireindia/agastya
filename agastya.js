2275281e5d9d6cdbfbd6bfdc718413ca522d284d
yogireindia <wacshortstudios@gmail.com>
2025-02-18 19:29:05 +0530

    Update agastya.js

--- agastya.js
+++ agastya.js
@@ -23,3 +23,14 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 });
+
+
+document.getElementById('contact-link').addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent the default anchor behavior
+    document.getElementById('contactus').scrollIntoView({ behavior: 'smooth' });
+});
+
+document.getElementById('about-link').addEventListener('click', function (e) {
+    e.preventDefault(); // Prevent the default anchor behavior
+    document.getElementById('mukulsection').scrollIntoView({ behavior: 'smooth' });
+});
